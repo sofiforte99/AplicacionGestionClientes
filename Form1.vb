cbdbc69c2c19fe6878e2301ad744f30b7c49e214
sofiforte99 <64369371+sofiforte99@users.noreply.github.com>
2021-07-18 21:27:03 -0300

    Agregar archivos de proyecto.

---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Imports ClienteNegocio
+
+Public Class Form1
+    Dim negocio As New ClienteNegocio.PruebaConexion()
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        negocio.DatosClientes()
+
+    End Sub
+End Class
